679f7259e793417cf159ae2d99b525a73fb769e4
Md Rasedul Islam <coderraseed@gmail.com>
2023-07-03 08:03:21 +0600

    Now solve all problem

--- m18/app/Models/Category.php
+++ m18/app/Models/Category.php
@@ -2,10 +2,12 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
+    use HasFactory;
     protected $fillable = ['name'];
 
     public function posts()
--- m18/app/Models/posts.php
+++ m18/app/Models/posts.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class posts extends Model
 {
-
+    use HasFactory;
     use SoftDeletes;
 
     protected $fillable = ['title', 'content'];
